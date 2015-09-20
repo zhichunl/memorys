8f31f2563d50417f41f7625e513eb96333fea32f
Sophia Anopa <sanopa@gmail.com>
2015-09-20 02:41:14 -0400

    Pressing Enter searches.

--- popup.js
+++ popup.js
@@ -1,10 +1,18 @@
 $(function () {
   $("#filter").click(function(){
-    $("#pics").empty();
-    console.log("filter");
-    dumpPics($('#search').val());
+    search()
+  });
+  $("#search").keyup(function(event){
+    if (event.keyCode == 13) {
+      search();
+    }
   });
 });
+
+function search() {
+  $("#pics").empty();
+  dumpPics($('#search').val());
+}
 
 function openLink(uri) {
   chrome.tabs.create({url: uri});
@@ -14,16 +22,16 @@ function dumpPics(query){
     $.ajax({
         type: 'GET',
         url: 'http://memories-7.herokuapp.com/search?term='+query,
-        
+
     }).done(function(response) {
       var results = $.parseJSON(response);
-      
+
       total = "";
       for(var x in results) {
         total += '<img id="pic'+x+'" width=300px src='+results[x] + '/></a>';
       }
 
-      $('#pics').html(total);  
+      $('#pics').html(total);
       for(var x in results) {
         $('#pic'+x).click(function () {openLink(results[x])});
       }

f0e6398b3ef44d0434415664daf13e56bd4bba98
James Wu <jameswu@cmu.edu>
2015-09-20 01:15:56 -0400

    Changed json

--- popup.js
+++ popup.js
@@ -7,18 +7,13 @@ $(function () {
 });
 
 function dumpPics(query){
-    var xmlHttp = new XMLHttpRequest();
-    console.log(query);
-    xmlHttp.onreadystatchange = function () {
-      if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-      {
-        console.log(xmlHttp.responseText);
-        callback(xmlHttp.responseText);
-      }
-    }
-    var url = "https://memories-7.herokuapp.com/search?term=" + query;
-    xmlHttp.open("GET", url, true);
-    xmlHttp.send(null);
+    $.ajax({
+        type: 'GET',
+        url: 'http://memories-7.herokuapp.com/search?term='+query,
+        
+    }).done(function(response) {
+      console.log($.parseJSON(response));  
+    });
 }
 
 
